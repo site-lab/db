ba1b2882c1119ba9f623d29b281a23bd600a923e
林 <hayashi@site-lab.jp>
2021-02-15 16:04:39 +0900

    ファイル修正

--- mysql8.sh
+++ mysql8.sh
@@ -35,31 +35,18 @@ if [ -e /etc/redhat-release ]; then
     if [ $DIST = "redhat" ];then
       if [ $DIST_VER = "7" ];then
 
-        #rootユーザーのパスワード
-        RPASSWORD=$(more /dev/urandom  | tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ,.+\-\!' | fold -w 12 | grep -i [12345678] | grep -i '[,.+\-\!]' | head -n 1)
-        #userパスワード
-        UPASSWORD=$(more /dev/urandom  | tr -dc '12345678abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ,.+\-\!' | fold -w 12 | grep -i [12345678] | grep -i '[,.+\-\!]' | head -n 1)
-
         #EPELリポジトリのインストール
-        start_message
-        yum remove -y epel-release
-        yum -y install epel-release
-        end_message
+        wget wget https://www.logw.jp/download/shell/common/system/repository.sh
+        source ./repository.sh
 
-        #gitリポジトリのインストール
-        start_message
-        yum -y install git
-        end_message
-
+        #ユーザー作成
+        wget wget https://www.logw.jp/download/shell/common/user/dbuseradd.sh
+        source ./dbuseradd.sh
 
 
         # yum updateを実行
-
-        start_message
-        echo "yum updateを実行します"
-        echo ""
-        #yum -y update
-        end_message
+        wget wget https://www.logw.jp/download/shell/common/system/update.sh
+        source ./update.sh
 
         #MariaDBを削除
         start_message
@@ -68,131 +55,9 @@ if [ -e /etc/redhat-release ]; then
         rm -rf /var/lib/mysql/
         end_message
 
-        #公式リポジトリの追加
-        start_message
-        rpm -ivh https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm
-        yum info mysql-community-server
-        end_message
-
-        #MySQLのインストール
-        start_message
-        echo "MySQLのインストール"
-        echo ""
-        yum -y install mysql-community-server --enablerepo=mysql80-community
-        yum list installed | grep mysql
-        end_message
-
-        #バージョン確認
-        start_message
-        echo "MySQLのバージョン確認"
-        echo ""
-        mysql --version
-        end_message
-
-        #my.cnfの設定を変える
-        start_message
-        echo "ファイル名をリネーム"
-        echo "/etc/my.cnf.default"
-        mv /etc/my.cnf /etc/my.cnf.default
-
-        echo "新規ファイルを作成してパスワードを無制限使用に変える"
-        cat <<EOF >/etc/my.cnf
-# For advice on how to change settings please see
-# http://dev.mysql.com/doc/refman/8.0/en/server-configuration-defaults.html
-
-[mysqld]
-#
-# Remove leading # and set to the amount of RAM for the most important data
-# cache in MySQL. Start at 70% of total RAM for dedicated server, else 10%.
-# innodb_buffer_pool_size = 128M
-#
-# Remove the leading "# " to disable binary logging
-# Binary logging captures changes between backups and is enabled by
-# default. It's default setting is log_bin=binlog
-# disable_log_bin
-#
-# Remove leading # to set options mainly useful for reporting servers.
-# The server defaults are faster for transactions and fast SELECTs.
-# Adjust sizes as needed, experiment to find the optimal values.
-# join_buffer_size = 128M
-# sort_buffer_size = 2M
-# read_rnd_buffer_size = 2M
-#
-# Remove leading # to revert to previous value for default_authentication_plugin,
-# this will increase compatibility with older clients. For background, see:
-# https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_default_authentication_plugin
-# default-authentication-plugin=mysql_native_password
-
-datadir=/var/lib/mysql
-log-error=/var/log/mysqld.log
-socket=/var/lib/mysql/mysql.sock
-
-character-set-server = utf8mb4
-collation-server = utf8mb4_bin
-default_password_lifetime = 0
-
-#slowクエリの設定
-slow_query_log=ON
-slow_query_log_file=/var/log/mysql-slow.log
-long_query_time=0.01
-EOF
-        end_message
-
-        #自動起動
-        start_message
-        echo "MySQLの自動起動を設定"
-        echo ""
-        systemctl enable mysqld.service
-        end_message
-
-        #自動起動
-        start_message
-        echo "MySQLの起動"
-        echo ""
-        systemctl start mysqld.service
-        systemctl status mysqld.service
-        end_message
-        #パスワード設定
-        start_message
-        DB_PASSWORD=$(grep "A temporary password is generated" /var/log/mysqld.log | sed -s 's/.*root@localhost: //')
-        #sed -i -e "s|#password =|password = '${DB_PASSWORD}'|" /etc/my.cnf
-        mysql -u root -p${DB_PASSWORD} --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${RPASSWORD}'; flush privileges;"
-        echo ${RPASSWORD}
-
-cat <<EOF >/etc/createdb.sql
-CREATE DATABASE centos;
-CREATE USER 'centos'@'localhost' IDENTIFIED BY '${UPASSWORD}';
-GRANT ALL PRIVILEGES ON centos.* TO 'centos'@'localhost';
-FLUSH PRIVILEGES;
-SELECT user, host FROM mysql.user;
-EOF
-mysql -u root -p${RPASSWORD}  -e "source /etc/createdb.sql"
-
-        end_message
-
-        #ファイルを保存
-        cat <<EOF >/etc/my.cnf.d/centos.cnf
-[client]
-user = centos
-password = ${UPASSWORD}
-host = localhost
-EOF
-
-        systemctl restart mysqld.service
-
-        #ファイルの保存
-        start_message
-        echo "パスワードなどを保存"
-        cat <<EOF >/root/pass.txt
-root = ${RPASSWORD}
-centos = ${UPASSWORD}
-EOF
-        end_message
-
-
-        #cnfファイルの表示
-        cat /etc/my.cnf
-
+        #MySQL8.0の追加
+        wget wget https://www.logw.jp/download/shell/common/database/rdbm80.sh
+        source ./rdbm80.sh
         echo ""
         echo ""
         cat <<EOF
